593b33f2d946179a7c8bf0bed7405c5cf84465c9
Juling1878 <389566885@qq.com>
2022-07-08 17:04:01 +0800

    调整了，模型看起来没有那么黝黑了

--- src/views/threejsDemo/index.vue
+++ src/views/threejsDemo/index.vue
@@ -35,34 +35,38 @@ export default {
       //  创建场景对象Scene
       this.scene = new THREE.Scene();
 
-      //网格模型添加到场景中
-      let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new THREE.MeshNormalMaterial({
-        color: "white",
-      });
-      this.mesh = new THREE.Mesh(geometry, material);
-      // this.scene.add(this.mesh);
       //加坐标
-      let axisHelper = new THREE.AxisHelper(250);
-      this.scene.add(axisHelper);
+      // let axisHelper = new THREE.AxisHelper(250);
+      // this.scene.add(axisHelper);
 
       /**
        * 相机设置
        */
-      let container = document.getElementById("container");
+      let container = document.getElementById("container")
       this.camera = new THREE.PerspectiveCamera(
-        70,
+        45,
         container.clientWidth / container.clientHeight,
-        0.01,
-        100
+        1,
+        2000
       );
-      this.camera.position.z = 1;
+      this.camera.position.z = 250;
+
+      //加灯光
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+      this.scene.add(ambientLight);
+
+      const pointLight = new THREE.PointLight(0xffffff, 0.8);
+      this.camera.add(pointLight);
+      this.scene.add(this.camera);
 
       /**
        * 创建渲染器对象
        */
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
-      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      this.renderer = new THREE.WebGLRenderer();
+      // 这两个比较关键，不然就黑黢黢的
+      this.renderer.outputEncoding = THREE.sRGBEncoding;
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(this.renderer.domElement);
 
       //创建控件对象
@@ -76,32 +80,6 @@ export default {
       // this.mesh.rotation.y += 0.02;
       this.renderer.render(this.scene, this.camera);
     },
-    // 加载obj文件
-    // loadOBJ() {
-    //   let _this = this;
-    //   _this.objLoader = new OBJLoader();
-    //   // const materialScene = new THREE.MeshBasicMaterial({ color: 0x008000 });
-    //   _this.objLoader.load(
-    //     "/static/male02.obj",
-    //     function(obj) {
-    //       // obj.material = materialScene;
-    //       //模型缩放
-    //       obj.scale.set(0.003, 0.003, 0.003);
-    //       // console.log("obj", obj);
-    //       _this.scene.add(obj);
-    //       // 光照
-    //       let ambient = new THREE.AmbientLight(0x008000);
-    //       _this.scene.add(ambient);
-    //       _this.animate();
-    //     },
-    //     function() {
-    //       console.log("导入模型成功");
-    //     },
-    //     function() {
-    //       console.log("导入模型失败");
-    //     }
-    //   );
-    // },
     // 加载mtl文件
     loadMTL() {
       const onProgress = function (xhr) {
@@ -124,20 +102,21 @@ export default {
               function (object) {
                 console.log("objec", object);
                 // 模型缩放
-                object.scale.set(0.003, 0.003, 0.003);
-                object.position.set(0, -0.3, 0.5);
+                // object.scale.set(0.003, 0.003, 0.003);
+                // object.position.set(0, -0.3, 0.5);
+                object.position.y = -95;
                 // object.position.y = -95;
                 _this.scene.add(object);
                 // 光照
-                const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-                _this.scene.add(ambientLight);
+                // const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+                // _this.scene.add(ambientLight);
                 // const pointLight = new THREE.PointLight(0xffffff, 1);
                 // _this.camera.add(pointLight);
 
-                const directionalLight  = new THREE.DirectionalLight(0xffffff);
-                directionalLight.position.set(0, 40, 40); //点光源位置
-                _this.scene.add(directionalLight); //点光源添加到场景中
-                _this.camera.lookAt(_this.scene.position);
+                // const directionalLight = new THREE.DirectionalLight(0xffffff);
+                // directionalLight.position.set(0, 40, 40); //点光源位置
+                // _this.scene.add(directionalLight); //点光源添加到场景中
+                // _this.camera.lookAt(_this.scene.position);
                 _this.animate();
               },
               onProgress
